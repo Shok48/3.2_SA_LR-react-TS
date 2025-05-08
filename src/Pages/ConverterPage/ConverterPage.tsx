a87287d4b5f9a6b7c8d2bbcd1c4f7b802ec5ff19
Shok48 <145995576+Shok48@users.noreply.github.com>
2025-05-08 20:20:33 +0300

    Создана страница первой лабораторной работы (Конвертер)

---
+++ src/Pages/ConverterPage/ConverterPage.tsx
@@ -0,0 +1,13 @@
+import BasePage from "../BasePage/BasePage";
+
+const ConverterPage = () => {
+    return (
+        <BasePage 
+            title='Конвертер левых инцидентов в матрицы смежности и инцидентности'
+        >
+
+        </BasePage>
+    )
+}
+
+export default ConverterPage;
